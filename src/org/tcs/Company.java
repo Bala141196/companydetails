484ec77ea7958cf656efe7e8ef4c028a18210d70
bala141196 <balac141196@gmail.com>
2022-04-21 21:50:11 +0530

    message

---
+++ src/org/tcs/Company.java
@@ -0,0 +1,16 @@
+package org.tcs;
+
+import org.client.Client;
+
+public class Company extends Client {
+	public void companyname() {
+System.out.println("tcs");
+}
+public static void main(String[] args) {
+	Company c=new Company();
+	c.clientname();
+	c.companyname();
+}
+ 
+
+}
