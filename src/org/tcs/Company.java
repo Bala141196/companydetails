5ee2a0f916a7cba4ac556a5cf2552ed26b3611eb
bala141196 <balac141196@gmail.com>
2022-04-21 22:00:50 +0530

    code modified

--- src/org/tcs/Company.java
+++ src/org/tcs/Company.java
@@ -5,6 +5,7 @@ import org.client.Client;
 public class Company extends Client {
 	public void companyname() {
 System.out.println("tcs");
+System.out.println("cts");
 }
 public static void main(String[] args) {
 	Company c=new Company();
